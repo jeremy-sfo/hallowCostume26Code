a387f74802f024ee1d5c8321eb34c70dacf20d7f
jeremy <jeremyyshen@gmail.com>
2026-08-13 20:39:31 -0700

    strange bugs appeared

--- Animations.ino
+++ Animations.ino
@@ -1,21 +1,11 @@
 void characterBlinkingAnimation(){
-  Serial.println(nextDelayTime);
+  //Serial.println(nextDelayTime);
 
   u8g2.clearBuffer();
-  u8g2.drawXBMP(6, 16, 120, 50, blinkFace3);
+  u8g2.drawXBMP(6, 16, 120, 50, blinkFace1);
   u8g2.sendBuffer();
 
-  waitSumTime(5);
-
-  u8g2.clearBuffer();
-  u8g2.drawXBMP(6, 16, 120, 50, blinkFace4);
-  u8g2.sendBuffer(); 
-
-  waitSumTime(5);
-
-  u8g2.clearBuffer();
-  u8g2.drawXBMP(6, 16, 120, 50, blinkFace3);
-  u8g2.sendBuffer(); 
+ // waitSumTime(4);
 }
 
 void characterLookAroundAnimation(){
